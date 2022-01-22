ef1f980cabb366365999e13649fa86dbb11e98a2
Alejandro Juarez <luisalex944@gmail.com>
2022-01-21 18:56:24 -0600

    agrega docs

---
+++ docs/precache-manifest.30c83e55e68fb27d8dd46d65f01d23d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6247f733d0e25dec5092f1aede44161",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6dacb7fbfa26176e474",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6dacb7fbfa26176e474",
+    "url": "/static/js/main.a65302ac.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
